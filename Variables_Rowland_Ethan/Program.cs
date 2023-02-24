a2ca89695f35cf8959e66545a242d23434658787
generalrowland <ethandale0916@gmail.com>
2023-02-24 13:00:12 -0600

    update

--- Variables_Rowland_Ethan/Program.cs
+++ Variables_Rowland_Ethan/Program.cs
@@ -6,7 +6,25 @@ namespace Variables_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declares an integer to store a favorite number
+            int favoriteNumber;
+            // Bolleans intialized to false for jumping and running
+            bool isJumping = false, isRunning = false;
+            // Declared random float variable
+            float myFloat;
+            // Assigning the favoriteNumber to 6
+            favoriteNumber = 6;
+            // Assigning myFloat to 54.65
+            myFloat = 54.65f;
+            // Intializing constant finalGrade to 90
+            const double finalGrade = 90.0;
+
+            // Prints all variables to console
+            Console.WriteLine("Favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myFloat);
+            Console.WriteLine("Final Grade: " + finalGrade);
         }
     }
 }
